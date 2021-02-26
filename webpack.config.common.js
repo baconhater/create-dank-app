6cd0d9b0c45e444dce12c65a0749dc70158c03d7
Enzo <enzoozne69@gmail.com>
2021-02-26 23:13:25 +0000

    add theme files/dev dependencies to json

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = {
   target: 'web',
@@ -81,9 +80,5 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new MonacoWebpackPlugin({
-      // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
-      languages: ['json'],
-    }),
   ],
 };
